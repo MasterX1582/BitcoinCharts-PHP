1958888451e165989e235fdff33fe4ab988b00b2
MasterX1582 <davy1582@msn.com>
2014-03-24 19:03:23 +0100

    Update bitcoinCharts.php

--- bitcoinCharts.php
+++ bitcoinCharts.php
@@ -2,15 +2,12 @@
 /*
  * Bitcoin Charts API Library
  * 
- * @author:   Brandon Beasley <http://brandonbeasley.com/>
- * @modifier  Davy Moedbeck <http://www.megax.be/>
- * @copyright Copyright (C) 2011 Brandon Beasley
- * @license   GNU GENERAL PUBLIC LICENSE (Version 3, 29 June 2007)
- * 
  *          Please consider donating if you use this library:
- *
- *              1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
- *              1PPkz4tQepxyXiEf9xjyZS8cTkxN9Q6uPN
+ * @author:   Brandon Beasley <http://brandonbeasley.com/> --> 1PPkz4tQepxyXiEf9xjyZS8cTkxN9Q6uPN
+ * @modifier  Davy Moedbeck <http://www.megax.be/> --> 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
+ * @copyright Copyright (C) 2011 Brandon Beasley
+ * @modiright Modiright (M) 2014 Davy Moedbeck
+ * @license   GNU GENERAL PUBLIC LICENSE (Version 3, 29 June 2007)
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
